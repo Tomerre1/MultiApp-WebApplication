068fc94240d6b04fd12be9eb79982f4e6fb911f7
Tomer <tomerevach@gmail.com>
2021-08-25 16:15:20 +0300

    over flow css for body

--- cmps/Email/EmailList.jsx
+++ cmps/Email/EmailList.jsx
@@ -1,13 +1,33 @@
-
+import { emailService } from "../../services/Email/email.service.js"
+import { EmailPreview } from "./EmailPreview.jsx"
 export class EmailList extends React.Component {
-  
+    state = {
+        emails: null
+    }
+
+
+    componentDidMount() {
+        this.loadEmails();
+    }
+
+    loadEmails() {
+        emailService.query().then((emails) => {
+            this.setState({ emails });
+        });
+    }
+
     render() {
+        const { emails } = this.state
+        if (!emails) return <div> Loading </div>
         return (
             <section className="list">
                 <div className="search-list flex">
                     <input type="search" placeholder='Search a mail' />
                 </div>
                 <div className="mail-list">
+                    {emails.map((mail) => {
+                        return <EmailPreview key={mail.id} email={mail} />
+                    })}
                 </div>
             </section>
         )
--- pages/Email/EmailApp.jsx
+++ pages/Email/EmailApp.jsx
@@ -2,32 +2,9 @@ const Router = ReactRouterDOM.HashRouter
 const { Route, Switch } = ReactRouterDOM
 import { EmailNav } from "../../cmps/Email/EmailNav.jsx"
 import { EmailList } from "../../cmps/Email/EmailList.jsx"
-import { emailService } from "../../services/Email/email.service.js"
+
 export class EmailApp extends React.Component {
-    state = {
-        emails: null
-    }
-
-
-    componentDidMount() {
-        this.loadEmails();
-    }
-
-    loadEmails() {
-        // debugger;
-        // console.log(emailService)
-        emailService.query().then((emails) => {
-            this.setState({ emails });
-        });
-    }
-
-
-
     render() {
-        const {emails} = this.state
-        console.log('%c  emails:', 'color: #0e93e0;background: #aaefe5;', emails);
-
-        if(!emails) return <div> Loading </div>
         return (
             <Router>
                 <main className="email-app">
