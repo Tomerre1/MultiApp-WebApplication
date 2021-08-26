b9669bbde06c61cdf121fdd37d7e94ec391cec06
Tomer <tomerevach@gmail.com>
2021-08-27 00:12:11 +0300

    modal styling

--- cmps/Email/EmailAdd.jsx
+++ cmps/Email/EmailAdd.jsx
@@ -32,17 +32,14 @@ export class EmailAdd extends React.Component {
             <section className="modal-compose-email">
                 <button className="closeModal" onClick={this.props.toggleCompose}>&times;</button>
                 <form className="compose-email flex" onSubmit={this.onSubmitMail} >
-                    <h2> Compose </h2>
+                    <h2 class="mail-title"> Compose </h2>
                     <div className="mail-to flex-column">
-                        <label htmlFor="to">To</label>
-                        <input autoFocus id="to" name="to" onChange={this.handleChange} type="text" placeholder="Enter email" required />
+                        <input autoFocus id="to" name="to" onChange={this.handleChange} type="email" placeholder="Enter email" required />
                     </div>
                     <div className="mail-subject flex-column">
-                        <label htmlFor="subject">Subject</label>
                         <input onChange={this.handleChange} id="subject" name="subject" type="text" placeholder="Enter subject" required />
                     </div>
                     <div className="mail-body flex-column">
-                        <label htmlFor="body">Message</label>
                         <textarea onChange={this.handleChange} type="text" name="body" id="textarea" placeholder='Enter Text' rows="7" cols="50" required /><br />
                     </div>
                     <div className="add-mail-btn-container">
