b931185a407b4c375aeecefb16f57324f98bb5e1
Tomer <tomerevach@gmail.com>
2021-08-27 17:58:42 +0300

    gmail compose button

--- cmps/Email/EmailNav.jsx
+++ cmps/Email/EmailNav.jsx
@@ -24,7 +24,7 @@ export class EmailNav extends React.Component {
         return (
             <nav>
                 <ul className="email-side-nav clean-list flex align-items">
-                    <NavLink activeClassName="active-nav-email" className="compus-mail" onClick={this.props.toggleCompose}><i className="fas fa-plus"></i>Compose</NavLink>
+                    <NavLink activeClassName="active-nav-email" className="compus-mail" onClick={this.props.toggleCompose}> Compose</NavLink>
                     <NavLink activeClassName="active-nav-email" onClick={() => { this.props.onSetFilter({ ['text']: '', ['isRead']: null, ['isStar']: null, status: 'inbox' }) }} to={`/email/inbox`}><i className="fas fa-inbox"></i>Inbox</NavLink>
                     <NavLink activeClassName="active-nav-email" onClick={() => { this.props.onSetFilter({ ['text']: '', status: 'sent' }) }} to={`/email/sent`}><i className="far fa-paper-plane"></i>Sent</NavLink>
                     <NavLink activeClassName="active-nav-email" onClick={() => { this.props.onSetFilter({ ['text']: '', ['isRead']: true }) }} to={`/email/read`}><i className="fas fa-sign-out-alt"></i>Read</NavLink>
