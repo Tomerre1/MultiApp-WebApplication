f5ae8b800b1a094aadc9f33928ac6528803c2ee6
Tomer <tomerevach@gmail.com>
2021-08-27 23:51:20 +0300

    home page responsive-design

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,8 +1,32 @@
 export class Home extends React.Component {
     render() {
         return (
-            <div>
-            </div>
+            <section>
+                <div className="hero">
+                    <div className="background-image" ></div>
+                    <div className="hero-content-area">
+                        <h1 className="app-name">MultiApp</h1>
+                        <h3> Everything you need is right here.</h3>
+                    </div>
+                </div>
+                <div className="try-it-now">
+                    <h3 className="hero-title">Loved by over 10 million users around the world!!!</h3>
+                    <div className="img-container">
+                        <div className="tablets">
+                            <img src="../assets/img/book-tablet.png"/>
+                            <button className="try-it-now-btns">Try Books</button>
+                        </div>
+                        <div className="tablets">
+                            <img src="../assets/img/gmail-tablet.png" />
+                            <button className="try-it-now-btns">Try Mail</button>
+                        </div>
+                        <div className="tablets">
+                            <img src="../assets/img/note-tablet.png" />
+                            <button className="try-it-now-btns">Try Keep</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
         )
     }
 }
