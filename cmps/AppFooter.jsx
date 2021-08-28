cf920465966594d1a8e62778e95ca33e8e22aa41
Tomer <tomerevach@gmail.com>
2021-08-28 11:06:43 +0300

    fixed footer responsive

--- cmps/AppFooter.jsx
+++ cmps/AppFooter.jsx
@@ -6,12 +6,12 @@ export class AppFooter extends React.Component {
             <footer>
                 <div className="contact-us">
                     <a target="_blank" href="https://il.linkedin.com/in/tomer-revah-software-engineering" className="fab fa-linkedin"></a>
-                    <a target="_blank" href="https://il.linkedin.com/in/tomer-revah-software-engineering" className="name">Tomer Revah</a>
+                    <a target="_blank" href="https://il.linkedin.com/in/tomer-revah-software-engineering" className="name">Tomer</a>
                 </div>
-                <h1 className="app-name">MultiApp</h1>
+                <a className="app-name">MultiApp</a>
                 <div className="contact-us">
                     <a target="_blank" href="https://www.linkedin.com/in/matan-levi-561115199/" className="fab fa-linkedin"></a>
-                    <a target="_blank" href="https://www.linkedin.com/in/matan-levi-561115199/" className="name">Matan Levi</a>
+                    <a target="_blank" href="https://www.linkedin.com/in/matan-levi-561115199/" className="name">Matan</a>
                 </div>
             </footer>
         )
