60cd97cac9a02b9ed15cef6a1384f1282f819065
Tomer <tomerevach@gmail.com>
2021-08-28 13:09:38 +0300

    navigation bug fixed email app

--- cmps/Email/EmailPreview.jsx
+++ cmps/Email/EmailPreview.jsx
@@ -22,23 +22,33 @@ export class EmailPreview extends React.Component {
         this.props.loadEmails()
     }
 
+    getCurrentStatus = () => {
+        const { email } = this.state
+        if (email.status === 'sent' && email.status !== 'trash') return 'sent'
+        else if (email.isStar && email.status !== 'trash') return 'star'
+        else if (email.isRead && email.status !== 'trash') return 'read'
+        else if (!email.isRead && email.status !== 'trash') return 'unread'
+        else if (email.status === 'trash') return 'trash'
+        else return 'inbox'
+    }
+
 
 
     render() {
         const { email } = this.state
         if (!email) return <div></div>
         return (
-            <Link to={`/email/${email.status}/${email.id}`}>
+            <Link to={`/email/${this.getCurrentStatus()}/${email.id}`}>
                 <section className={`mail-preview flex space-between ${(!email.isRead) ? 'read' : 'unread'}`} onClick={this.onSetRead}  >
                     <div className="flex sender">
                         <div>
 
-                        <button className="star-btn" onClick={(event) => { event.preventDefault(); this.props.onSetStar(email) }}>
-                            <i className={`fas fa-star ${(email.isStar) ? 'active' : ''}`} aria-hidden="true" ></i>
-                        </button>
-                        <button className="remove-btn" onClick={(event) => { event.preventDefault(); this.props.onRemoveEmail(email.id) }}>
-                            <i className="fas fa-trash" aria-hidden="true"></i>
-                        </button>
+                            <button className="star-btn" onClick={(event) => { event.preventDefault(); this.props.onSetStar(email) }}>
+                                <i className={`fas fa-star ${(email.isStar) ? 'active' : ''}`} aria-hidden="true" ></i>
+                            </button>
+                            <button className="remove-btn" onClick={(event) => { event.preventDefault(); this.props.onRemoveEmail(email.id) }}>
+                                <i className="fas fa-trash" aria-hidden="true"></i>
+                            </button>
                         </div>
                         <p>{emailService.getLoggedInUser().fullName}</p>
                     </div>
